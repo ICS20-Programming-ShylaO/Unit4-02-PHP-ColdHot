b9fcc0bd7492e04720e292a8ad3ee2ad5607ea12
ShylaOommen <124379324+ShylaOommen@users.noreply.github.com>
2023-04-18 21:25:45 +0000

    more linter error fixing

--- results.php
+++ results.php
@@ -1,16 +1,17 @@
 <?php
+
 // initializing variable $userTemp
 
   $userTemp = $_POST['user-temp'];
 
 // if $userTemp >= 20, display "Wow, it's hot today!"
 
-  if ($userTemp >= 20) {
+if ($userTemp >= 20) {
       echo "Wow, it's hot today!";
-  }
+} 
 
 // else, display "Looks like it's cold today."
 
-  else {
+else {
       echo "Looks like it's cold today.";
-  } 
+} 
